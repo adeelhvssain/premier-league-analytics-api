225662d25d1df0b5c5cb2c9819ed50ff0564f095
Adeel Hussain <adeelh.004@gmail.com>
2026-03-10 01:00:47 +0000

    added basic match API endpoints

--- app/main.py
+++ app/main.py
@@ -7,8 +7,10 @@ from .database import engine
 from .models.base import Base
 from .models.player import Player
 from .models.team import Team
+from .models.match import Match
 from .routers.teams import router as teams_router
 from .routers.players import router as players_router
+from .routers.matches import router as matches_router
 
 
 def create_app() -> FastAPI:
@@ -42,6 +44,7 @@ def create_app() -> FastAPI:
     # Extension point:
     app.include_router(teams_router)
     app.include_router(players_router)
+    app.include_router(matches_router)
 
     return app
 
